b151a425778d2261928dba85ad6fe7874285e308
Юра Ананьин <viplod@yandex.ru>
2022-04-13 23:01:31 +0500

    add logger

--- homework.py
+++ homework.py
@@ -1,23 +1,30 @@
-# import logging
+import logging
 import os
 import time
 import requests
 
 from dotenv import load_dotenv
+from logging.handlers import StreamHandler
 
 import telegram
 
-# from telegram import ReplyKeyboardMarkup
-# from telegram.ext import CommandHandler, Updater
-
 load_dotenv()
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log',
+    format='%(asctime)s, %(levelname)s, %(message)s'
+)
+
+handler = StreamHandler('my_logger.log', maxBytes=50000000, backupCount=5)
+logging.addHandler(handler)
 
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 20
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -32,20 +39,39 @@ HOMEWORK_STATUSES = {
 def send_message(bot, message):
     """Отправка сообщения."""
     bot.send_message(TELEGRAM_CHAT_ID, message)
+    logging.info('Сообщение отправлено в Telegram')
 
 
 def get_api_answer(current_timestamp):
     """Запрос данных с сервера."""
     timestamp = current_timestamp or int(time.time())
-    # params = {'from_date': timestamp}
-    params = {'from_date': 0}
+    params = {'from_date': timestamp}
     response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    return response.json()
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise ValueError
 
 
 def check_response(response):
     """Проверка ответа сервера."""
-    return response.get('homeworks')
+    if not response:
+        logging.error('Отсутствие ожидаемых ключей в ответе API')
+        raise ValueError
+    if isinstance(response, dict):
+        homework = response.get('homeworks')
+    else:
+        logging.error('Отсутствие ожидаемых ключей в ответе API')
+        raise TypeError
+    if isinstance(homework, list):
+        if response.get('homeworks') is not None:
+            return response.get('homeworks')
+        else:
+            logging.error('Отсутствие ожидаемых ключей в ответе API')
+            raise KeyError
+    else:
+        logging.error('Отсутствие ожидаемых ключей в ответе API')
+        raise TypeError
 
 
 def parse_status(homework):
@@ -53,11 +79,26 @@ def parse_status(homework):
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
     verdict = HOMEWORK_STATUSES.get(homework_status)
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    if verdict is not None:
+        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    else:
+        raise KeyError
 
 
 def check_tokens():
     """Проверка наличия токенов."""
+    if PRACTICUM_TOKEN is None:
+        logging.critical('Отсутствует обязательная переменная окружения:'
+                         '"PRACTICUM_TOKEN"'
+                         'Программа принудительно остановлена.')
+    if TELEGRAM_TOKEN is None:
+        logging.critical('Отсутствует обязательная переменная окружения:'
+                         '"TELEGRAM_TOKEN"'
+                         'Программа принудительно остановлена.')
+    if TELEGRAM_CHAT_ID is None:
+        logging.critical('Отсутствует обязательная переменная окружения:'
+                         '"TELEGRAM_CHAT_ID"'
+                         'Программа принудительно остановлена.')
     return PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID
 
 
@@ -66,18 +107,16 @@ def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     if not check_tokens():
-        return
+        raise ValueError
     while True:
         try:
             response = get_api_answer(current_timestamp)
-            # print(response)
+            print(response)
             current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            print(message)
-#           logging
             time.sleep(RETRY_TIME)
         else:
             homeworks = check_response(response)
